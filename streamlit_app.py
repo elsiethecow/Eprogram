0c131c32360aab09bebe529d62c4e08ec808dc08
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2022-02-19 20:00:40 -0500

    used lottie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
+
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+def lottie(url):
+    content = load_lottieurl(url)
+    st_lottie(content)
 
 st.balloons()
 
-if st.button("Please push me"):
+st.title("love you")
+
+if st.button("Please push me"):   
+
+    lottie("https://assets10.lottiefiles.com/packages/lf20_n1a5ebk9.json")
+    lottie("https://assets6.lottiefiles.com/packages/lf20_wzKgBd.json")
     ":crown::dog::christmas_tree:"*900
